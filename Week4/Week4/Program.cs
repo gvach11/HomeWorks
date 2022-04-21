b714fe7c07ddbb9cf5ac86e6f389c5987674dee9
Giorgi Vachnadze <gvach11@gmail.com>
2022-04-21 23:17:11 +0400

    Week 4 Homework Commit onsole.BackgroundColor = ConsoleColor.Blue;#

---
+++ Week4/Week4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Week4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Giorgi Vachnadze");
+            Console.Write("Please enter text: ");
+            var userInput = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("-------------------------");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Your text: " + userInput);
+            Console.ResetColor();
+        }
+    }
+}

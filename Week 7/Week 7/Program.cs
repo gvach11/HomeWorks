66246dcd430efd82600f32c05211a15baf6e9ed3
Giorgi Vachnadze <giorgi.vachnadze@singular.uk>
2022-05-06 11:31:13 +0400

    task 2 done

--- Week 7/Week 7/Program.cs
+++ Week 7/Week 7/Program.cs
@@ -34,6 +34,30 @@ namespace Week_7
                     
                 }
 
+                //task 2
+
+                if (taskNumber == "2")
+                {
+                    Console.WriteLine("This is task 2");
+                    Console.WriteLine("Please enter array size");
+                    var arraySizeInput = Console.ReadLine();
+                    int arraySize;
+                    bool arraySizeCheck = int.TryParse(arraySizeInput, out arraySize);
+                    if (arraySizeCheck is true) {
+                        var jackpotArray = new string[arraySize];
+                        for (int i = 0; i < jackpotArray.Length; i++)
+                        {
+                            Console.WriteLine($"Please enter element #{i+1}");
+                            jackpotArray[i] = Console.ReadLine();
+                        }
+                        bool jackpotCheck = jackpotArray.GroupBy(x => x).Count()==1;
+                        if (jackpotCheck is true) Console.WriteLine("YES");
+                        else Console.WriteLine("NO");
+                    }
+                    else { Console.WriteLine("Please enter a whole number"); }
+
+                }
+
 
 
 

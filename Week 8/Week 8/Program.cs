aa339792b2063de3b7ff0acf292d53f25b8ef66e
Giorgi Vachnadze <giorgi.vachnadze@singular.uk>
2022-05-11 13:03:22 +0400

    all tasks done

--- Week 8/Week 8/Program.cs
+++ Week 8/Week 8/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Week_8
@@ -20,7 +21,7 @@ namespace Week_8
         }
         static void TaskChooser(string taskInput)
         {
-
+            //Task 1
             if (taskInput == "1")
             {
                 Console.WriteLine("Please enter a");
@@ -31,6 +32,7 @@ namespace Week_8
                 int n = Convert.ToInt32(Console.ReadLine());
                 Console.WriteLine(Task1(a, b, n));
             }
+            //Task 2
             if (taskInput == "2")
             {
                 Console.WriteLine("Please enter the list of characters to check for pairs");
@@ -38,19 +40,64 @@ namespace Week_8
                 var outputString = Task2(inputString);
                 Console.WriteLine($"There are {outputString} pairs in your string");
             }
+            //Task 3
             if (taskInput == "3")
             {
                 Console.WriteLine("Please input string #1");
                 string stringOne = Console.ReadLine();
                 Console.WriteLine("Please enter string #2");
                 string stringTwo = Console.ReadLine();
-                Console.WriteLine(Task3(stringOne, stringTwo));
+                Console.WriteLine($"The common last characters are: {Task3(stringOne, stringTwo)}");
             }
+            //Task 4
             if (taskInput == "4")
             {
-                var stringlist = new string[2] {"S", "d"};
-                Console.WriteLine(Task4(stringlist));
+                Console.WriteLine("What sort of list would you like? [1] - String, [2] - Integer, [3] - Boolean");
+                var arrayChoice = Console.ReadLine();
+                if (arrayChoice == "1")
+                {
+                    var stringlist = new List<string>();
+                    Console.WriteLine("How many elements would you like?");
+                    int elementChoice = Convert.ToInt32(Console.ReadLine());
+                    for(int i = 0; i < elementChoice; i++)
+                    {
+                        Console.WriteLine($"Please enter element {i + 1}");
+                        stringlist.Add(Console.ReadLine());
+                    }
+
+                    Task4(stringlist);
+                }
+                if (arrayChoice == "2")
+                {
+                    var intlist = new List<int>();
+                    Console.WriteLine("How many elements would you like?");
+                    int elementChoice = Convert.ToInt32(Console.ReadLine());
+                    for (int i = 0; i < elementChoice; i++)
+                    {
+                        Console.WriteLine($"Please enter element {i + 1}");
+                        intlist.Add(Convert.ToInt32(Console.ReadLine()));
+                    }
+
+                    Task4(intlist);
+                }
+                if (arrayChoice == "3")
+                {
+                    var boollist = new List<bool>();
+                    Console.WriteLine("How many elements would you like?");
+                    int elementChoice = Convert.ToInt32(Console.ReadLine());
+                    for (int i = 0; i < elementChoice; i++)
+                    {
+                        Console.WriteLine($"Please enter element {i + 1}, [1] for true, [0] for false");
+                        var boolinput = Console.ReadLine();
+                        if (boolinput == "1") { boollist.Add(true); }
+                        if (boolinput == "0") { boollist.Add(false); }
+                        
+                    }
+                    Task4(boollist);
+                }
+                
             }
+            //Task 5
             if (taskInput == "5")
             {
                 Console.WriteLine("Enter a number");
@@ -134,24 +181,54 @@ namespace Week_8
             return endResult;
             }
 
-        static T Task4 <T>(T value)
+        static void Task4<T>(List<T> someList) 
         {
-            
-            return value;
+
+           
+            if (typeof(T) == typeof(string))
+            {
+                foreach(var item in someList) {
+                    var itemUpper = item.ToString().ToUpper();
+                    Console.WriteLine(itemUpper); }
+            }
+            if (typeof(T) == typeof(int))
+            {
+                int sum = 0;
+                foreach (var item in someList)
+                {
+                    sum += Convert.ToInt32(item); 
+                    
+                }
+                Console.WriteLine(sum);
+            }
+            if (typeof (T) == typeof(bool))
+            {
+                Console.WriteLine($"The first element is {someList[0]}");
+                Console.WriteLine($"The middle element is {someList[someList.Count/2]}");
+                Console.WriteLine($"The last element is {someList[someList.Count-1]}");
+            }
+
             
         }
+
 
         static void Task5(int a)
         {
             string converted = Convert.ToString(a);
-            Console.Write(converted.Substring(0,1));
+            Console.Write($"{converted.Substring(0, 1)}-");
             converted = converted.Substring(1, converted.Length-1);
-            if (converted.Length > 0)
+            if (converted.Length >1)
             {
                 int reconverted = Convert.ToInt32(converted);
                 Task5(reconverted);
             }
-            else { Console.WriteLine("\n"); }
+            else if(converted.Length > 0) 
+            {
+                int reconverted = Convert.ToInt32(converted);
+                Console.Write($"{converted.Substring(0, 1)}");
+                Console.WriteLine("\n");
+            }
+            
 
 
 

9ce3481a5aec7db85b46129184cf202b33dd2479
Giorgi Vachnadze <giorgi.vachnadze@singular.uk>
2022-05-18 16:16:54 +0400

    both tasks done

---
+++ Week 10/Week 10/Microfinance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_10
+{
+    public class Microfinance : IFinanceOperations
+    {
+
+        public bool CheckUserHistory()
+        {
+            return true;
+        }
+        public void CalculateLoanPercent(int month)
+        {
+        }
+
+        public void CalculateLoanPercent(int totalAmount, int month)
+        {
+            var result = totalAmount + (totalAmount * (0.1 * month)) + (4*month);
+            Console.WriteLine($"The total amount to pay is {result}");
+
+
+        }
+
+    }
+}
--- Week 10/Week 10/TaskChooser.cs
+++ Week 10/Week 10/TaskChooser.cs
@@ -23,6 +23,36 @@ namespace Week_10
                 FileWorkerExecute.Edit(extension, maxSize);
                 FileWorkerExecute.Delete(extension, maxSize);
             }
+
+            if(input == "2")
+            {
+                Console.WriteLine("Please select [1] for Bank or [2] for Microfinance");
+                var financechoice = Console.ReadLine();
+                if(financechoice == "1") 
+                {
+                    Bank bank = new Bank();
+                    Console.WriteLine($"History check returned {bank.CheckUserHistory()}");
+                    Console.WriteLine("Please enter the loan amount");
+                    var amount = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please enter the number of months");
+                    var months = Convert.ToInt32(Console.ReadLine());
+                    bank.CalculateLoanPercent(amount, months);
+                }
+                else if (financechoice == "2")
+                {
+                    Microfinance microfinance = new Microfinance();
+                    Console.WriteLine($"History check returned {microfinance.CheckUserHistory()}");
+                    Console.WriteLine("Please enter the loan amount");
+                    var amount = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Please enter the number of months");
+                    var months = Convert.ToInt32(Console.ReadLine());
+                    microfinance.CalculateLoanPercent(amount, months);
+                }
+                
+
+                
+                
+            }
         }
     }
 }

7a80f4fde6ac41741c47a265fe1525c40ffbad64
Giorgi Vachnadze <giorgi.vachnadze@singular.uk>
2022-05-18 15:29:07 +0400

    task 1 done

---
+++ Week 10/Week 10/FileWorker.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_10
+{
+    public abstract class FileWorker
+    {
+        int maxFileSize { get; set; }
+        abstract public string extension { get; set; }
+
+        public abstract void Read();
+        public abstract void Write();
+        public abstract void Edit();
+        public abstract void Delete();
+        
+
+    }
+    public class FileWorkerExecute : FileWorker
+    {
+        public override string extension { get; set; }
+
+        public override void Delete()
+        {
+        }
+        public static void Delete(string inputstring, int inputint)
+        {
+            Console.WriteLine($"I can read from {inputstring} file with max storage of {inputint} ");
+        }
+
+        public override void Edit()
+        {
+        }
+
+        public static void Edit(string inputstring, int inputint)
+        {
+            Console.WriteLine($"I can edit {inputstring} file with max storage of {inputint} ");
+        }
+
+        public override void Read() 
+        {
+        }
+        public static void Read(string inputstring, int inputint)
+        {
+            Console.WriteLine($"I can read from {inputstring} file with max storage of {inputint} ");
+        }
+
+        public override void Write()
+        {
+        }
+
+        public static void Write(string inputstring, int inputint)
+        {
+            Console.WriteLine($"I can write to {inputstring} file with max storage of {inputint} ");
+
+        }
+    }
+}
---
+++ Week 10/Week 10/TaskChooser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_10
+{
+    public class TaskChooser
+    {
+        string input { get; set; }
+
+        public static void Chooser(string input) 
+        {
+            if (input == "1")
+            {
+                Console.WriteLine("Please enter the file extension");
+                var extension = Console.ReadLine();
+                Console.WriteLine("Please enter max size");
+                var maxSize = Convert.ToInt32(Console.ReadLine());
+                FileWorkerExecute.Read(extension, maxSize);
+                FileWorkerExecute.Write(extension, maxSize);
+                FileWorkerExecute.Edit(extension, maxSize);
+                FileWorkerExecute.Delete(extension, maxSize);
+            }
+        }
+    }
+}

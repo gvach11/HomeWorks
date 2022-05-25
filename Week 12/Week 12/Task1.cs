258b9aff96c32f11f0cc6f40a10a95d78d4a82ec
Giorgi Vachnadze <giorgi.vachnadze@singular.uk>
2022-05-25 16:38:32 +0400

    task 1 done

---
+++ Week 12/Week 12/Task1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Week_12
+{
+    public class Task1
+    {
+        public string filepath = @"C:\task1.txt";
+        public void CreateOrClear()
+        {
+            if (!File.Exists(filepath))
+            {
+                var myfile = File.Create(filepath);
+                myfile.Close();
+            }
+            else
+            {
+                File.WriteAllText(filepath, String.Empty);
+            }
+        }
+        public void WriteToFile(int lineNumber)
+        {
+            for (int i = 0; i < lineNumber; i++)
+            {
+                File.AppendAllText(filepath, $"This is line #{(i+1).ToString()}\n");
+            }
+            Console.WriteLine(File.ReadLines(filepath).Last());
+            
+        }
+    }
+}
---
+++ Week 12/Week 12/TaskChooser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_12
+{
+    public class TaskChooser
+    {
+        string input { get; set; }
+
+        public static void Chooser(string input)
+        {
+            if (input == "1")
+            {
+                Console.WriteLine("Please inpu the number of lines");
+                var lineNumber = Convert.ToInt32(Console.ReadLine());
+                Task1 fileworker = new Task1();
+                fileworker.CreateOrClear();
+                fileworker.WriteToFile(lineNumber);
+                
+            }
+        }
+    }
+}

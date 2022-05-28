0f064f8a3f8262c5b8fe4c74c03681fcbb58c71c
Giorgi Vachnadze <giorgi.vachnadze@singular.uk>
2022-05-29 00:00:23 +0400

    form is done and saving to file is working, need to do json formatting now

---
+++ Week13/Week13/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Week13.Models;
+using System.IO;
+
+namespace Week13.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [Route("/app")]
+        public ActionResult Appointment()
+        {
+
+            return View();
+
+        }
+        [HttpPost]
+        public ActionResult Create(Microsoft.AspNetCore.Http.IFormCollection Collection)
+        {
+            string filepath = @"C:\Week13.json";
+            if (!System.IO.File.Exists(filepath))
+            {
+                var myfile = System.IO.File.Create(filepath);
+                myfile.Close();
+            }
+            if (ModelState.IsValid){
+                AppointmentModel appointment = new AppointmentModel();
+                appointment.FirstName = Collection["FirstName"].ToString();
+                System.IO.File.WriteAllText(filepath, appointment.FirstName);
+            }
+            return RedirectToAction("Appointment");
+        }
+    }
+}
---
+++ Week13/Week13/Models/AppointmentModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Week13.Models
+{
+    public class AppointmentModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Doctor { get; set; }
+        public DateTime Time { get; set; }
+    }
+}

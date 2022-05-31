81123b39cf2e00eae8de375ecd971d885bb130fd
Giorgi Vachnadze <giorgi.vachnadze@singular.uk>
2022-05-31 18:39:53 +0400

    registration done, validation to do

---
+++ Week14/Week14/Controllers/PersonController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Week14.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : Controller
+    {
+        private readonly string _filePath = Directory.GetCurrentDirectory() + "\\Week14.json";
+
+        [HttpPost ("register")]
+        public IActionResult Register([FromQuery]Person person)
+        {
+            var newPerson = new Person
+            {
+                CreateDate = DateTime.Now,
+                Firstname = person.Firstname,
+                Lastname = person.Lastname,
+                JobPosition = person.JobPosition,
+                Salary = person.Salary,
+                WorkExperince = person.WorkExperince,
+                PersonAddress = new Address
+                {
+                    City = person.PersonAddress.City,
+                    Country = person.PersonAddress.Country,
+                    HomeNumber = person.PersonAddress.HomeNumber
+                }
+            };
+
+            if (!System.IO.File.Exists(_filePath))
+            {
+                var myfile = System.IO.File.Create(_filePath);
+                myfile.Close();
+
+            }
+            string personString = JsonConvert.SerializeObject(newPerson);
+            System.IO.File.AppendAllText(_filePath, personString + "\n");
+            return Ok(person);
+        }
+    }
+}
---
+++ Week14/Week14/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Week14
+{
+    public class Person
+    {
+        internal DateTime CreateDate { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string JobPosition { get; set; }
+        public double Salary { get; set; }
+        public double WorkExperince { get; set; }
+        public Address PersonAddress { get; set; }
+    }
+
+    public class Address
+    {
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int HomeNumber { get; set; }
+    }
+}

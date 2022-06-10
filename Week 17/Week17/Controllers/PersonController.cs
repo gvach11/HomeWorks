3ff1d629f07b1d30e5fdf266da8ce23e287913cc
Giorgi Vachnadze <giorgi.vachnadze@singular.uk>
2022-06-10 17:49:59 +0400

    all tasks done

--- Week 17/Week17/Controllers/PersonController.cs
+++ Week 17/Week17/Controllers/PersonController.cs
@@ -68,111 +68,96 @@ namespace Week17.Controllers
             return CreatedAtAction("GetPersons", new { id = person.Id }, person);
         }
 
-        //Reading the whole file
+        //Reading the table
         [HttpGet ("getdata")]
-        public async Task<ActionResult<IEnumerable<Person>>> GetPersons()
+        public IQueryable<Object> GetPersons()
         {
-
-            return await _context.Persons.ToListAsync();
+            
+            return (from p in _context.Persons
+                    join a in _context.Addresses on p.Id equals a.PersonId
+                    select new
+                    {
+                        id = p.Id,
+                        create_date = p.CreateDate,
+                        first_name = p.Firstname,
+                        last_name = p.Lastname,
+                        job_position = p.JobPosition,
+                        salary = p.Salary,
+                        work_experience = p.WorkExperince,
+                        country = a.Country,
+                        city = a.City,
+                        home_number = a.HomeNumber
+                        
+                    }) ; //ამის უფრო მარტივი სინტაქსი არ არსებობს, select * -ის ვარიანტში რომ იყოს
+                         //დაჯოინებული თეიბლიდან? ვერ ვნახე და ხელით მაგიტომ დავწერე :D მარტო პერსონი რო მომქონდა
+                         //ადრესში null-ებს აბრუნებდა.
         }
 
         //Reading a certain line
         [HttpGet ("getdata/{id}")]
-        public IActionResult GetDataById(int id)
+        public IQueryable<Object> GetPersonById(int id)
         {
-            try
-            {
-                string readline = System.IO.File.ReadLines(_filePath).Skip(id).Take(1).First();
-                return Ok(readline);
-            }
-            catch (System.InvalidOperationException)
-            {
-                return NotFound($"The file does not containt index#{id}");
-            }
+            return (from p in _context.Persons
+                    join a in _context.Addresses on p.Id equals a.PersonId
+                    where p.Id == id
+                    select new
+                    {
+                        id = p.Id,
+                        create_date = p.CreateDate,
+                        first_name = p.Firstname,
+                        last_name = p.Lastname,
+                        job_position = p.JobPosition,
+                        salary = p.Salary,
+                        work_experience = p.WorkExperince,
+                        country = a.Country,
+                        city = a.City,
+                        home_number = a.HomeNumber
 
-            
+                    });
         }
 
         //Filter
         [HttpGet ("getdatabyparam")]
-        public IActionResult GetDataByQuery([FromQuery] Person person)
+        public IQueryable<Object> GetPersonByName([FromQuery] Person person)
         {
-            var personList = new List<Person>();
-            var linesRead = System.IO.File.ReadLines(_filePath);
-            foreach (var line in linesRead)
-            {
-                var jsonLine = JsonConvert.DeserializeObject<Person>(line);
-                personList.Add(jsonLine);
 
-            }
-            var result = personList.Where(x => x.Salary == person.Salary);
-            if (result.Count() == 0)
-            {
-                return Ok("No Results Found");
-            }
-            return Ok(result);
-            
+            return (from p in _context.Persons
+                    join a in _context.Addresses on p.Id equals a.PersonId
+                    where p.Firstname == person.Firstname
+                    select new
+                    {
+                        id = p.Id,
+                        create_date = p.CreateDate,
+                        first_name = p.Firstname,
+                        last_name = p.Lastname,
+                        job_position = p.JobPosition,
+                        salary = p.Salary,
+                        work_experience = p.WorkExperince,
+                        country = a.Country,
+                        city = a.City,
+                        home_number = a.HomeNumber
 
+                    });
         }
 
         //Delete
 
         [HttpDelete ("delete/{id}")]
-        public IActionResult DeleteById(int id)
+        public async Task<ActionResult<Person>> DeleteById(int id)
         {
-            var personList = new List<Person>();
-            var linesRead = System.IO.File.ReadLines(_filePath);
-            foreach (var line in linesRead)
-            {
-                var jsonLine = JsonConvert.DeserializeObject<Person>(line);
-                personList.Add(jsonLine);
-
-            }
-
-            try
-            {
-                personList.RemoveAt(id);
-                System.IO.File.WriteAllText(_filePath, String.Empty);
-                foreach (Person person in personList)
-                {
-                    var person2string = JsonConvert.SerializeObject(person);
-                    System.IO.File.AppendAllText(_filePath, person2string + "\n");
-                }
-                string readfile = System.IO.File.ReadAllText(_filePath);
-                return Ok(readfile);
-            }
-            catch (System.InvalidOperationException)
-            {
-                return NotFound($"The file does not containt index#{id}");
-            }
+            var currentRecord = _context.Persons.Find(id);
+            _context.Persons.Remove(currentRecord);
+            _context.SaveChanges();
+            return Ok(currentRecord);
         }
         [HttpPut("update/{id}")]
-        public IActionResult UpdateById(int id)
+        public async Task<ActionResult<Person>> UpdateById(int id, string fname)
         {
-            var personList = new List<Person>();
-            var linesRead = System.IO.File.ReadLines(_filePath);
-            foreach (var line in linesRead)
-            {
-                var jsonLine = JsonConvert.DeserializeObject<Person>(line);
-                personList.Add(jsonLine);
-
-            }
-            try
-            {
-                personList[id].Firstname = "ChangedName";
-                System.IO.File.WriteAllText(_filePath, String.Empty);
-                foreach (Person person in personList)
-                {
-                    var person2string = JsonConvert.SerializeObject(person);
-                    System.IO.File.AppendAllText(_filePath, person2string + "\n");
-                }
-                string readfile = System.IO.File.ReadAllText(_filePath);
-                return Ok(readfile);
-            }
-            catch (System.InvalidOperationException)
-            {
-                return NotFound($"The file does not containt index#{id}");
-            }
+            var currentRecord = _context.Persons.Find(id);
+            currentRecord.Firstname = fname;
+            _context.Persons.Update(currentRecord);
+            _context.SaveChanges();
+            return Ok(currentRecord);
 
         }
 
